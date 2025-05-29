e4989f21cc3ec42722ffe5ca87188ffe58169049
Bryan Inacio <bryan.inacio@aluno.senai.br>
2025-05-29 16:50:47 -0300

    feat: tela home finalizada

--- event/src/pages/home/Home.jsx
+++ event/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import "./Home.css"
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer"
+
 import Banner from "../../assets/img/Banner.svg"
 import Mapa from "../../assets/img/Mapa.svg"
+import Visao from "../../assets/img/Visao.svg"
+
 
 const Home = () => {
     return (
@@ -13,8 +16,6 @@ const Home = () => {
                 <section className="home_section">
                     <div className="pagina_home">
                         <img src={Banner} alt="" />
-                        <hr />
-                        <p>Área de eventos da Escola de Informática</p>
                     </div>
 
                     <div className="proximos_eventos">
@@ -23,7 +24,7 @@ const Home = () => {
                             <hr />
                         </div>
 
-                        <div className="lista_eventos">
+                        <div className="lista_eventos layout_grid">
                             <div className="item">
                                 <h1>Titulo do Evento</h1>
 
@@ -59,14 +60,8 @@ const Home = () => {
                     </div>
 
                     <div className="visao">
-                        <div className="titulo_2">
-                            <h1>Visão</h1>
-                            <hr />
-                        </div>
-
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <img src={Visao} alt="" />
                     </div>
-
 
                     <div className="contato">
                         <div className="titulo_2">
@@ -74,14 +69,16 @@ const Home = () => {
                             <hr />
                         </div>
 
-                        <div className="mapa">
-                            <img src={Mapa} alt="" />
-                        </div>
+                        <div className="mapa_informacoes layout_grid">
+                            <div className="mapa">
+                                <img src={Mapa} alt="" />
+                            </div>
 
-                        <div className="informacoes_contato">
-                            <p>Rua Niterói, 180 - Centro</p>
-                            <p>São Caetano  do  Sul - SP</p>
-                            <p>(11) 4225-2000</p>
+                            <div className="informacoes_contato">
+                                <p>Rua Niterói, 180 - Centro</p>
+                                <p>São Caetano  do  Sul - SP</p>
+                                <p>(11) 4225-2000</p>
+                            </div>
                         </div>
                     </div>
                 </section>
